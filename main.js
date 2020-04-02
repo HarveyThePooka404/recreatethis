53c79469bf7caa01196fa37ac329491a8fd9d731
HarveyThePooka404 <57190763+HarveyThePooka404@users.noreply.github.com>
2020-04-02 20:09:12 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,27 @@
+
+window.addEventListener("DOMContentLoaded", getData);
+
+function getData() {
+    fetch("http://owldesign.dk/wordpress/wp-json/wp/v2/house")
+        .then(e => e.json())
+        .then(sortData);
+}
+
+function sortData (f) {
+    f.forEach((i)=> {
+
+    console.log(i);
+
+    const template = document.querySelector("template").content;
+    const clone = template.cloneNode(true);
+
+    clone.
+    clone.querySelector(".street_address").textContent = i.address;
+    clone.querySelector(".city_address").textContent = i.city;
+    clone.querySelector(".number_bed").textContent = i.bed;
+    clone.querySelector(".number_bath").textContent = i.bath;
+    clone.querySelector(".number_square").textContent = i.space;
+
+    document.querySelector("main").appendChild(clone);
+})};
+
